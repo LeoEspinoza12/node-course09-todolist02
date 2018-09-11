5660e2ba0b7db38f9116ecb271b33c6859879c87
Leo espinoza <leo.spinoza@yahoo.com>
2018-09-11 12:34:41 -0500

    added

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const hbs = require('express-handlebars')
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// const hbs = require('express-handlebars')
 
 
 
